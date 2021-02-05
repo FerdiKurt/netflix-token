7c4334a1d175d749c82b8260957d12a4080076c0
FerdiKurtSecurrency <ferdi.kurt111@gmail.com>
2021-02-05 09:39:33 +0300

    ignore this.

--- contracts/ANetflixToken.sol
+++ contracts/ANetflixToken.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity >=0.6.2 <0.8.0;
 
-import './ERC721.sol';
+import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import './NXTokenStorage.sol';
 
 abstract contract ANetflixToken is NXTokenStorage, ERC721 {
